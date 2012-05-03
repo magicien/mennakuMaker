ecbed59393c0f0271423988ddb353a0b41300d9a
quramy <yosuke.kurami@gmail.com>
2012-05-03 22:55:25 +0900

    replace files.

--- web.js
+++ web.js
@@ -3,14 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port);
-/*
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	  console.log("Listening on " + port);
 });
-*/
-
