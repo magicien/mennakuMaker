eceb678126141162510cf0db9d518717ffa6f0d6
magicien <darkhorse2.0spec+magicien@gmail.com>
2012-05-03 22:25:49 +0900

    edit web.js

--- web.js
+++ web.js
@@ -7,7 +7,10 @@ app.get('/', function(request, response) {
 });
 
 var port = process.env.PORT || 3000;
+app.listen(port);
+/*
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
 
