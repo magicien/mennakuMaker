3996d9b27deeca8124755d3d023d894fb07768d9
isano <straight.will@gmail.com>
2012-05-12 22:47:28 +0900

    refactoring

--- public/javascripts/copymsg.js
+++ public/javascripts/copymsg.js
@@ -1,5 +1,11 @@
-function copymsg() {
-	var msg = document.getElementById('copymsg').value;
-	document.getElementById('ccopy').innerHTML = msg;
+$('#copymsg').blur(function(){
+	$('#ccopy').html($('#copymsg').val());
+});
 
-}
+/*
+$(function(){
+	//var $('TODO ID').val($('#ccopy').css('-webkit-transform')
+	var transform = $('#ccopy').css('-webkit-transform');
+
+});
+*/
