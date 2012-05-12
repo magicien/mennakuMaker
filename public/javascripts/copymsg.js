7d17bf7d95d78927f36856099e538422b67fe5fc
isano <straight.will@gmail.com>
2012-05-12 23:45:21 +0900

    enter taiou

--- public/javascripts/copymsg.js
+++ public/javascripts/copymsg.js
@@ -2,6 +2,13 @@ $('#copymsg').blur(function(){
 	$('#ccopy').html($('#copymsg').val());
 });
 
+
+$('#copymsg').keypress(function(e){
+	if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+		$('#ccopy').html($('#copymsg').val());
+	}
+});
+
 /*
 $(function(){
 	//var $('TODO ID').val($('#ccopy').css('-webkit-transform')
