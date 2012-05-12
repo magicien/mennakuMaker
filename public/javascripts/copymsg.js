bb131512d1cf61aaf28f45b6ed62d596d426eeac
isano <straight.will@gmail.com>
2012-05-13 00:48:06 +0900

    commit

--- public/javascripts/copymsg.js
+++ public/javascripts/copymsg.js
@@ -1,3 +1,5 @@
+$(function() {
+
 $('#copymsg').blur(function(){
 	$('#ccopy').html($('#copymsg').val());
 });
@@ -16,3 +18,28 @@ $(function(){
 
 });
 */
+
+	$('#ccopy').css('position', 'relative');
+	$('#ccopy').css('left', trans.left+200+'px');
+	$('#ccopy').css('top', trans.top-600+'px');
+	$('#ccopy').css('fontSize', decoration.fontSize+'px');
+	$('#ccopy').css('fontColor', '#'+decoration.color);
+	$('#ccopy').css('text-shadow', 
+			decoration.textShadow.x+'px '+decoration.textShadow.y+'px '+decoration.textShadow.blur+'px #'+decoration.textShadow.color);
+	$('#ccopy').css('-webkit-writing-mode', 'vertical-rl');
+	$('#ccopy').css('-webkit-transform', 'rotate\('+$('#input_rotate').val()+'deg\)');
+
+	$('#input_left').blur(function(){
+		$('#ccopy').css('left', parseInt($('#input_left').val())+200+'px');
+	});
+
+	$('#input_top').blur(function(){
+		$('#ccopy').css('top', parseInt($('#input_top').val())-600+'px');
+	});
+	
+	$('#input_rotate').change(function(){
+		$('#ccopy').css('-webkit-transform','rotate\('+$('#input_rotate').val()+'deg\)');
+	});
+
+});
+
